41e1f7b31e93a7d490063da64ca359b21578e54a
laura <laura@LAPTOP-JGULSF8U>
2022-12-26 19:57:05 +0100

    commit

--- RENOS/src/RENOS/Start.java
+++ RENOS/src/RENOS/Start.java
@@ -6,17 +6,17 @@ public class Start {
 		
 		
 		//Creamos los dos Robot´s
-		Robot Peleon = new Robot();
-		Peleon.setNombre("Firulais");
-		Peleon.setAtaque();
-		Peleon.setDefensa();
-		Peleon.setPuntosVida();
+		Robot Peleon = new Robot("Firulais", 100);
 		
-		Robot Bola = new Robot();
-		Bola.setNombre("Beni");
-		Bola.setAtaque();
-		Bola.setDefensa();
-		Bola.setPuntosVida();
+		Peleon.setAtaque(10);
+		Peleon.setDefensa(50);
+	
+		
+		Robot Bola = new Robot("Beni",85);
+	
+		Bola.setAtaque(15);
+		Bola.setDefensa(80);
+		
 
 	
 		
