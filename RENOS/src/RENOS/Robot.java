1219bdc19bb7f6e9e9e719522576c352cbc729a4
laura <laura@LAPTOP-JGULSF8U>
2022-12-26 17:08:59 +0100

    commit

---
+++ RENOS/src/RENOS/Robot.java
@@ -0,0 +1,73 @@
+package RENOS;
+
+public class Robot {
+	
+	//ATRIBUTOS
+	private String nombre;
+	private float puntosVida;
+	private float ataque;
+	private float defensa;
+	
+	
+	//CONSTRUCTORES
+	public Robot (String nombre,float puntosVida) {
+		//Son valores que no varian
+		this.nombre=nombre;
+		this.puntosVida= 100;
+		/* 
+		 * Valores aleatorios
+		this.ataque=generarAtaque(); 
+		this.defensa=generarDefensa();
+		*/
+	}
+	
+	private float generarAtque () {
+		
+		float ataque= ;
+		
+		int fuerzaAtaque= 0;
+		
+		
+	
+	}
+	
+	//GETTERS
+	public String getNombre() {
+		return nombre;
+	}
+	public float getPuntosVida() {
+		return puntosVida;
+	}
+	public float getAtaque() {
+		return ataque;
+	}
+	public float getDefensa() {
+		return defensa;
+	}
+	
+	
+	//SETTERS
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public void setPuntosVida(float puntosVida) {
+		this.puntosVida = puntosVida;
+	}
+	public void setAtaque(float ataque) {
+		this.ataque = ataque;
+	}
+	public void setDefensa(float defensa) {
+		this.defensa = defensa;
+	}
+	
+	
+	public String toString() {
+		return "Robot [nombre=" + nombre + ", puntosVida=" + puntosVida + ", ataque=" + ataque + ", defensa=" + defensa
+				+ "]";
+	}
+	
+	
+
+}
+
+
---
+++ RENOS/src/RENOS/Start.java
@@ -0,0 +1,26 @@
+package RENOS;
+
+public class Start {
+
+	public static void main(String[] args) {
+		
+		
+		//Creamos los dos Robot´s
+		Robot Peleon = new Robot();
+		Peleon.setNombre("Firulais");
+		Peleon.setAtaque();
+		Peleon.setDefensa();
+		Peleon.setPuntosVida();
+		
+		Robot Bola = new Robot();
+		Bola.setNombre("Beni");
+		Bola.setAtaque();
+		Bola.setDefensa();
+		Bola.setPuntosVida();
+
+	
+		
+
+	}
+
+}
